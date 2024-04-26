1cfa80a127d03f36b56c109e9e36af551f8f67f7
Xavier <xavier.figari@le-campus-numerique.fr>
2024-04-26 09:43:50 +0200

    cleanup

--- src/DD/Menu.java
+++ src/DD/Menu.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import static java.awt.SystemColor.menu;
-
 public class Menu {
 
     private String getPlayerName(Scanner sc) {
@@ -33,47 +31,6 @@ public class Menu {
         return new Person(getPlayerName(sc), getPlayerType(sc));
     }
 
-    private String getAnswerFromMainMenu(Scanner sc) {
-        String answer;
-        System.out.println("=========================");
-        System.out.println("      MENU PRINCIPAL     ");
-        System.out.println(" Que voulez-vous faire ? ");
-        System.out.println("=========================");
-        System.out.println("C. Créer un personnage");
-        System.out.println("A. Afficher les personnages déjà créés");
-        System.out.println("Q. Quitter le jeu");
-        System.out.print("\nEntrez C, A ou Q : ");
-        answer = sc.nextLine();
-        return answer;
-    }
-
-    private String getAnswerFromPlayerMenu(Scanner sc) {
-        String answer;
-        System.out.println(" ");
-        System.out.println("   Que voulez-vous faire maintenant ?\n");
-        System.out.println("   │ A. Afficher le personnage créé");
-        System.out.println("   │ M. Modifier le personnage");
-        System.out.println("   │ R. Revenir au menu principal");
-        System.out.println("   │ Q. Quitter le jeu");
-        System.out.print(  "\n   Entrez A, M, R ou Q : ");
-        answer = sc.nextLine();
-        return answer;
-    }
-
-    private void playerMenu(Scanner sc, List<Person> people) {
-        String answer;
-        do {
-            answer = getAnswerFromPlayerMenu(sc);
-            switch (answer) {
-                case "A": displayPlayer(people.getLast()); break;
-                case "M": modifyPlayer(people.getLast(), sc); break;
-                case "R": System.out.println("   Retour au menu principal :\n"); break;
-                case "Q": quit(); break;
-                default: System.out.print("Entrez A, M, R ou Q : ");
-            }
-        } while (! answer.equals("R"));
-    }
-
     private void quit() {
         System.out.println("Bye bye...");
         System.exit(0);
@@ -90,6 +47,42 @@ public class Menu {
         System.out.println("\n" + person);
     }
 
+    private void displayPlayerMenu() {
+        System.out.println(" ");
+        System.out.println("   Que voulez-vous faire maintenant ?\n");
+        System.out.println("   │ A. Afficher le personnage créé");
+        System.out.println("   │ M. Modifier le personnage");
+        System.out.println("   │ R. Revenir au menu principal");
+        System.out.println("   │ Q. Quitter le jeu");
+        System.out.print(  "\n   Entrez A, M, R ou Q : ");
+    }
+
+    private void playerMenu(Scanner sc, List<Person> people) {
+        String answer;
+        do {
+            displayPlayerMenu();
+            answer = sc.nextLine();
+            switch (answer) {
+                case "A": displayPlayer(people.getLast()); break;
+                case "M": modifyPlayer(people.getLast(), sc); break;
+                case "R": System.out.println("   Retour au menu principal :\n"); break;
+                case "Q": quit(); break;
+                default: System.out.print("Entrez A, M, R ou Q : ");
+            }
+        } while (! answer.equals("R"));
+    }
+
+    private void displayMainMenu() {
+        System.out.println("=========================");
+        System.out.println("      MENU PRINCIPAL     ");
+        System.out.println(" Que voulez-vous faire ? ");
+        System.out.println("=========================");
+        System.out.println("C. Créer un personnage");
+        System.out.println("A. Afficher les personnages déjà créés");
+        System.out.println("Q. Quitter le jeu");
+        System.out.print("\nEntrez C, A ou Q : ");
+    }
+
     public void mainMenu(Scanner sc) {
 //        Menu menu = new Menu();
         Game game = new Game();
@@ -97,7 +90,8 @@ public class Menu {
         List<Person> people = new ArrayList<Person>();
 
         do {
-            answer = getAnswerFromMainMenu(sc);
+            displayMainMenu();
+            answer = sc.nextLine();
             switch (answer) {
                 case "C": // Create player
                     // Add player (interactively)
